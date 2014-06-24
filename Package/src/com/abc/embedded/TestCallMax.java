a819c813c5bc619a6525848fb6a8ce2adaa926d0
Bryan@iii <chunwei.liu@gmail.com>
2014-06-24 15:29:17 +0800

    這個範例說明使用package 以及 import 的法則

---
+++ Package/src/com/abc/embedded/TestCallMax.java
@@ -0,0 +1,15 @@
+package com.abc.embedded;
+
+import com.abc.util.*;
+
+public class TestCallMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int j=80;
+		
+		System.out.println("max is " + Max.max(i, j));
+	}
+
+}
---
+++ Package/src/com/abc/util/Max.java
@@ -0,0 +1,7 @@
+package com.abc.util;
+
+public class Max {
+	public static int max(int a,int b){
+		return a>b?a:b;
+	}
+}
