9777c978479ee29f08c948e489ac9f6f2d4bcfc5
Bryan@iii <chunwei.liu@gmail.com>
2014-06-24 14:18:18 +0800

    通常 物件的資料 宣告為private 函數宣告為public

---
+++ Person/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Person Tom = new Person();
+			Tom.setAge(34);
+			Tom.setName("Tom");
+			
+			System.out.println("My name is "+Tom.getName());
+			System.out.println("My age is "+Tom.getAge());
+	}
+
+}
---
+++ Person/src/Person.java
@@ -0,0 +1,22 @@
+
+public class Person {
+	private int age;
+	private String name;
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String myName){
+		name=myName;
+	}
+	
+	public void setAge(int age){
+		this.age=age;
+	}
+	
+}
