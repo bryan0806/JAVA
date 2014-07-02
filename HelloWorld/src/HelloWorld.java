1cac8fafed106af29f42bf7735070baf049389aa
Bryan@iii <chunwei.liu@gmail.com>
2014-07-02 11:51:07 +0800

    另外則是使用 try ... catch 來夾住此未處理exception位置

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,12 +2,17 @@ import java.io.*;
 
 public class HelloWorld {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("What is your name :");
 		String name = null;
-		name = console.readLine();
+		try {
+			name = console.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Hello " + name);
 	}
 
