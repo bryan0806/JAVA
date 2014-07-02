d48caaa14bb6610155f0ad134c6db1b8fdc1359f
Bryan@iii <chunwei.liu@gmail.com>
2014-07-02 11:48:32 +0800

    程式錯誤的地方 請看訊息 這是沒有處理exception 的錯誤

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("What is your name :");
+		String name = null;
+		name = console.readLine();
+		System.out.println("Hello " + name);
+	}
+
+}
